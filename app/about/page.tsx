f852dc9e1291b014db01473ce93d183ff4098979
Robert Von Der Becke <r.vonderbecke@thevacorp.com>
2024-09-13 00:19:50 -0500

    added api keys to vercel

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,27 @@ import React from "react";
 const page = () => {
   return (
     <>
- 
-      <div className="pt-[90px]">About us page</div>
+      <div className="pt-[90px]">
+        <h1>About Crossroad Community Center</h1>
+        <p>
+          Welcome to Crossroad Community Center, where sports, arts, and
+          community come together to create a place for everyone. At the heart
+          of everything we do is the belief that community involvement is the
+          foundation of our success. Over the last 20 years, we’ve learned that
+          when people come together, great things happen.
+        </p>
+        <h2>Our Mission</h2>
+        <p>
+          Our mission is to provide a space where individuals of all ages and
+          backgrounds can connect, grow, and thrive. Crossroad Community Center
+          is more than just a facility—it's a place where the community's effort
+          and spirit come alive. We worked hand-in-hand with local families,
+          businesses, and leaders to bring this vision to life. This center
+          stands as a symbol of what we can achieve when we unite for a common
+          goal: building a healthier, more vibrant community.
+        </p>
+        <h2>Programs for Everyone</h2>
+      </div>
     </>
   );
 };
