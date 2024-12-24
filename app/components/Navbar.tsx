a6cb3e5bbd392f8ba42a149d875265ba31c2f663
Robert Von Der Becke <teamvcorp@thevacorp.com>
2024-12-23 20:40:48 -0600

    fixed mobile navigation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <nav ref={navbarRef} className="bg-darkBlue py-3">
+    <nav ref={navbarRef} className="bg-darkBlue py-3 lg:max-h-[50px]">
       {/* Mobile Menu Toggle */}
       <div className="flex justify-between items-center px-4 md:hidden">
         <h1 className="text-white text-xl font-bold">Crossroad</h1>
@@ -41,26 +41,30 @@ const Navbar: React.FC = () => {
       {/* Navbar Menu */}
       <ul
         className={`${
-          isMobileMenuOpen ? "block" : "hidden"
-        } md:flex space-y-4 md:space-y-0 md:space-x-6 justify-center px-4 md:px-0 relative`}
+          isMobileMenuOpen ? "flex flex-col" : "hidden"
+        } md:flex space-y-4 md:space-y-0 md:space-x-6 justify-center text-center  px-4 md:px-0 relative `}
       >
         {/* Who We Are */}
-        <li className="relative">
-          <button
-            className="text-white flex items-center gap-2 text-lg hover:text-yellow-400 transition-colors duration-300 w-full md:w-auto"
-            onClick={() => handleDropdown("whoWeAre")}
-          >
-            Who We Are <IoIosArrowDown />
-          </button>
+       
+          <li className="relative">
+            <button
+              className="text-white flex items-center gap-2 text-lg hover:text-yellow-400 transition-colors duration-300 w-full md:w-auto pr-8"
+              onClick={() => handleDropdown("whoWeAre")}
+            >
+              Who We Are <IoIosArrowDown />
+            </button>
           {activeDropdown === "whoWeAre" && (
-            <ul className="absolute bg-white text-black mt-2 shadow-lg rounded w-48 md:static md:w-auto md:mt-0 md:shadow-none">
+            <ul className=" bg-white text-black mt-2 shadow-lg rounded w-42">
               <li className="hover:bg-darkBlue hover:text-white">
                 <Link href="/aboutus/mission" className="block px-4 py-2">
                   Mission
                 </Link>
               </li>
               <li className="hover:bg-darkBlue hover:text-white">
-                <Link href="/aboutus/mission#vision" className="block px-4 py-2">
+                <Link
+                  href="/aboutus/mission#vision"
+                  className="block px-4 py-2"
+                >
                   Vision
                 </Link>
               </li>
@@ -71,18 +75,19 @@ const Navbar: React.FC = () => {
               </li>
             </ul>
           )}
-        </li>
+          </li>
+        
 
         {/* Programs */}
         <li className="relative">
           <button
-            className="text-white text-lg flex items-center gap-2 hover:text-yellow-400 transition-colors duration-300 w-full md:w-auto"
+            className="text-white text-lg flex items-center gap-2 hover:text-yellow-400 transition-colors duration-300 w-full md:w-auto pr-8"
             onClick={() => handleDropdown("programs")}
           >
             Programs <IoIosArrowDown />
           </button>
           {activeDropdown === "programs" && (
-            <ul className="absolute bg-white text-black mt-2 shadow-lg rounded w-48 md:static md:w-auto md:mt-0 md:shadow-none">
+            <ul className="bg-white text-black mt-2 shadow-lg rounded w-42 ">
               <li className="hover:bg-darkBlue hover:text-white">
                 <Link href="/programs/homeschool" className="block px-4 py-2">
                   Homeschool +
@@ -120,13 +125,13 @@ const Navbar: React.FC = () => {
         {/* Get Involved */}
         <li className="relative">
           <button
-            className="text-white text-lg flex items-center gap-2 hover:text-yellow-400 transition-colors duration-300 w-full md:w-auto"
+            className="text-white text-lg flex items-center gap-2 hover:text-yellow-400 transition-colors duration-300 w-full md:w-auto pr-8"
             onClick={() => handleDropdown("getInvolved")}
           >
             Get Involved <IoIosArrowDown />
           </button>
           {activeDropdown === "getInvolved" && (
-            <ul className="absolute bg-white text-black mt-2 shadow-lg rounded w-48 md:static md:w-auto md:mt-0 md:shadow-none">
+            <ul className="bg-white text-black mt-2 shadow-lg rounded w-42">
               <li className="hover:bg-darkBlue hover:text-white">
                 <Link href="/volunteer" className="block px-4 py-2">
                   Volunteer
@@ -145,7 +150,7 @@ const Navbar: React.FC = () => {
         <li>
           <Link
             href="/contact"
-            className="text-white text-lg hover:text-yellow-400 transition-colors duration-300 block"
+            className="text-white text-lg hover:text-yellow-400 transition-colors duration-300 flex pr-8"
           >
             Contact
           </Link>
@@ -155,7 +160,7 @@ const Navbar: React.FC = () => {
         <li>
           <Link
             href="/impactpage"
-            className="text-white text-lg hover:text-yellow-400 transition-colors duration-300 block"
+            className="text-white text-lg hover:text-yellow-400 transition-colors duration-300 flex pr-8"
           >
             Impact
           </Link>
@@ -165,7 +170,7 @@ const Navbar: React.FC = () => {
         <li>
           <Link
             href="/supportpage"
-            className="text-white text-lg flex items-center gap-2 hover:text-yellow-400 transition-colors duration-300 block"
+            className="text-white text-lg flex items-center gap-2 hover:text-yellow-400 transition-colors duration-300"
           >
             Donate <IoMdHeart color="red" />
           </Link>
