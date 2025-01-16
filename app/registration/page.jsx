e02d8ce50e693ccc509fc9262ce8f5f9f7874698
Robert Von Der Becke <teamvcorp@thevacorp.com>
2025-01-15 21:24:48 -0600

    added spanish form

--- app/registration/page.jsx
+++ app/registration/page.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 const RegistrationPage = () => {
+  const [spanishForm, setSpanishForm] = useState(false);
+
   return (
-    <div>
-      <h1>Registration Page</h1>
-      <iframe
-        src="https://na4.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhCfz2wGBoikxpx4kmx0F5GwW38pGQlCiynIsSszNBrcNg7H1XDIqBwRS8bAvHfm57c*&hosted=false"
-        width="100%"
-        height="100%"
-        style={{ border: 0, overflow: 'hidden', minHeight: '500px', minWidth: '600px' }}
-      ></iframe>
+    <div className="p-4">
+      {spanishForm ? (
+        <h1 className="text-2xl font-bold mb-4">
+          Página de Registro: Debe completarse en una computadora de escritorio.
+        </h1>
+      ) : (
+        <h1 className="text-2xl font-bold mb-4">
+          Registration Page: Should be filled out on desktop.
+        </h1>
+      )}
+
+      <button
+        className="px-4 py-2 bg-blue text-white rounded mb-4"
+        onClick={() => setSpanishForm((prev) => !prev)}
+      >
+        {spanishForm ? "Show English Form" : "Forma Español"}
+      </button>
+      {!spanishForm && (
+        <iframe
+          src="https://na4.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhCfz2wGBoikxpx4kmx0F5GwW38pGQlCiynIsSszNBrcNg7H1XDIqBwRS8bAvHfm57c*&hosted=false"
+          width="100%"
+          height="100%"
+          className="border-0 overflow-hidden min-h-[500px] min-w-[600px]"
+        ></iframe>
+      )}
+
+      {spanishForm && (
+        <iframe
+          src="https://na4.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhAizQazdhBMN5khHDOBq_Ey50UUD0r4dk_KYWO2RUlHxJ1xF8b6QEBPdYfeuC43-l4*&hosted=false"
+          width="100%"
+          height="100%"
+          className="border-0 overflow-hidden min-h-[500px] min-w-[600px]"
+        ></iframe>
+      )}
     </div>
   );
 };
